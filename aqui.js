0c4d36ff9f850878b00138c6479d141baecf2820
Cletche  Nafantchamna <61561998+nafantchamna@users.noreply.github.com>
2020-07-16 18:35:11 -0300

    Add files via upload

---
+++ aqui.js
@@ -0,0 +1,13 @@
+
+function alterarEstilo ()
+{
+    document.querySelector('#h1').classList.add('alterar');
+}
+
+function alterarEstiloAntigo() 
+{
+    document.querySelector('#h1').classList.remove('alterar');
+}
+
+
+
